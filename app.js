455666297ecb9343cf3bf6b26eb2ac55f0b2607c
Cody Usher <usher.cody@outlook.com>
2020-12-06 21:57:16 -0700

    Found images for my game

---
+++ app.js
@@ -0,0 +1,17 @@
+/* https://i.gifer.com/5bzm.gif -- Door Opening Gif
+   
+   https://mir-s3-cdn-cf.behance.net/project_modules/disp/46c03419676595.562de69f28142.jpg -- Hallway 1
+   
+   https://mir-s3-cdn-cf.behance.net/project_modules/disp/1f1d1519676595.562de69f188fe.jpg -- Hallway 2
+
+   https://mir-s3-cdn-cf.behance.net/project_modules/disp/214c6619676595.562de69f44825.jpg -- Hallway 3
+   
+   https://www.dsogaming.com/wp-content/uploads/2019/02/Negative-Atmosphere-1-1038x576.jpg -- enemy room 1
+   
+   https://www.relyonhorror.com/wp-content/uploads/2016/09/syndrome.jpg -- enemy room 2
+   
+   https://onlysp.escapistmagazine.com/wp-content/uploads/2016/08/Syndrome-Banner-800x400.jpg -- enemy room 3
+   
+   https://i.pinimg.com/564x/38/03/8f/38038f87a7a46ce32f9c24fbb8159782.jpg -- Boss Room
+   
+   https://static.wikia.nocookie.net/arrow/images/b/bb/Kara_Danvers%27_pod.png/revision/latest/scale-to-width-down/1000?cb=20170113123409 -- Escape Pod*/
