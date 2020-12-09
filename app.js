8064d08772a114a8ab1f78a3e51ccce7a1c4e686
Cody Usher <usher.cody@outlook.com>
2020-12-08 21:07:02 -0700

    got the random cards to work and be clickable. Start button also works

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+class Character {
+  constructor(dmg, name) {
+    this.hp = 100;
+    this.sp = 100;
+    this.dmg = dmg;
+    this.name = name;
+  }
+
+  attack(enemy) {
+    enemy.hp -= this.dmg;
+    this.sp -= 10;
+  }
+
+  drawCard() {
+    const $li = $("<li>")
+      .addClass("card")
+      .on("click", (event) => {
+        event.currentTarget.remove();
+        $(".maze-image").remove();
+        chosenDirection();
+        hero.drawCard();
+      })
+      .append($randomDirectionCard())
+      .appendTo($hand);
+  }
+}
+
 const mazeImages = [
   "https://mir-s3-cdn-cf.behance.net/project_modules/disp/46c03419676595.562de69f28142.jpg",
   "https://mir-s3-cdn-cf.behance.net/project_modules/disp/1f1d1519676595.562de69f188fe.jpg",
@@ -12,30 +39,45 @@ const directionCards = [
   "https://i.imgur.com/VXorAnC.png", //Left
 ];
 
-const handArray = [];
-
 const $hand = $("#hand");
 const $mainScreen = $("#main-screen");
+const $startScreen = $("#start-screen");
+const $startButton = $("#start-button");
 
+const $randomDirectionCard = () =>
+  $("<img>").attr(
+    "src",
+    directionCards[Math.floor(Math.random() * directionCards.length)]
+  );
 
-const changeMainScreen = () => {
-    const $randomMazeImage = $("<img>").attr("src", mazeImages[Math.floor(Math.random() * mazeImages.length)]);
-    $($mainScreen).append($randomMazeImage);
+const chosenDirection = () => {
+  const $randomMazeImage = $("<img>")
+    .attr("src", mazeImages[Math.floor(Math.random() * mazeImages.length)])
+    .addClass("maze-image");
+  $($mainScreen).append($randomMazeImage);
 };
 
 const createHand = () => {
-    const $randomDirectionCard = $("<img>").attr("src", directionCards[Math.floor(Math.random() * directionCards.length)]);
-    
-    for (cards of directionCards) {
-        const $li = $("<li>")
-            .append($randomDirectionCard)
-            .appendTo($hand);
-    };
-}
+  for (cards of directionCards) {
+    const $li = $("<li>")
+      .addClass("card")
+      .on("click", (event) => {
+        event.currentTarget.remove();
+        $(".maze-image").remove();
+        chosenDirection();
+        hero.drawCard();
+      })
+      .append($randomDirectionCard())
+      .appendTo($hand);
+  }
+};
 
-$(() => {
-    changeMainScreen();
-    createHand();
-    createHand();
-    createHand();
+const hero = new Character(5, "Cody");
+
+$startButton.on("click", () => {
+  $startScreen.remove();
+  createHand();
+  chosenDirection();
 });
+
+$(() => {});
