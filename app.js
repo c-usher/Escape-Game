92a9b1f7bdd73d79355f718b3ad0086116ab9e4f
Cody Usher <usher.cody@outlook.com>
2020-12-10 12:39:53 -0700

    combat is now working. Images have been updated and changed.

--- app.js
+++ app.js
@@ -26,17 +26,17 @@ class Character {
 }
 
 const mazeImages = [
-  "https://mir-s3-cdn-cf.behance.net/project_modules/disp/46c03419676595.562de69f28142.jpg",
-  "https://mir-s3-cdn-cf.behance.net/project_modules/disp/1f1d1519676595.562de69f188fe.jpg",
-  "https://mir-s3-cdn-cf.behance.net/project_modules/disp/214c6619676595.562de69f44825.jpg",
-  "https://www.dsogaming.com/wp-content/uploads/2019/02/Negative-Atmosphere-1-1038x576.jpg", //Battle Background
-  "https://www.relyonhorror.com/wp-content/uploads/2016/09/syndrome.jpg", //Battle Background
-  "https://onlysp.escapistmagazine.com/wp-content/uploads/2016/08/Syndrome-Banner-800x400.jpg", //Battle Background
+  "defaultImages/hallwayOne.jpg",
+  "defaultImages/hallwayTwo.jpg",
+  "defaultImages/hallwayThree.jpg",
+  "defaultImages/battleImageOne.jpg",
+  "defaultImages/battleImageTwo.jpg",
+  "defaultImages/battleImageThree.jpg"
 ];
 const directionCards = [
-  "https://i.imgur.com/fJqvBNO.png", //Forward
-  "https://i.imgur.com/XzulilU.png", //Right
-  "https://i.imgur.com/VXorAnC.png", //Left
+  "defaultImages/pointUpCard.png", //Forward
+  "defaultImages/pointingRightCard.png", //Right
+  "defaultImages/pointingLeftCard.png", //Left
 ];
 const currentMazeImage = [];
 const enemyInCombat = [];
@@ -105,16 +105,25 @@ const combat = () => {
   $(".attacked").text('Your Being Attacked!');
   enemyInCombat[0].attack(hero);
   if (hero.hp <= 0) {
-    alert("You Lose!")
+    $("<h1>").text("You Died!").appendTo($mainScreen);
   }
   if (enemyInCombat[0].hp <= 0) {
     for (index of enemyInCombat) {
       enemyGraveyard.push(index);
     };
     enemyInCombat.pop();
-    alert("You Killed Him");
+    console.log(enemyInCombat);
+    winCondition();
+    $("<h1>").text("You Killed Him!").appendTo($mainScreen);
   }
 };
+
+const winCondition = () => {
+  if (enemyGraveyard.length === 5) {
+    
+  }
+  
+}
 
 const hero = new Character(100, 5, "Cody");
 
@@ -126,7 +135,7 @@ $startButton.on("click", () => {
 
 $attackButton.on("click", () => {
   hero.attack(enemyInCombat[0]);
-  combat();//commit
+  combat();
 });
 
 
