1ff4b7bc4a8b47487ab5bb964dbbcc0173165888
Cody Usher <usher.cody@outlook.com>
2020-12-09 20:23:20 -0700

    Generate Enemy and attack enemy only on attack maze backgrounds

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class Character {
-  constructor(dmg, name) {
-    this.hp = 100;
+  constructor(hp, dmg, name) {
+    this.hp = hp;
     this.sp = 100;
     this.dmg = dmg;
     this.name = name;
@@ -29,20 +29,26 @@ const mazeImages = [
   "https://mir-s3-cdn-cf.behance.net/project_modules/disp/46c03419676595.562de69f28142.jpg",
   "https://mir-s3-cdn-cf.behance.net/project_modules/disp/1f1d1519676595.562de69f188fe.jpg",
   "https://mir-s3-cdn-cf.behance.net/project_modules/disp/214c6619676595.562de69f44825.jpg",
-  "https://www.dsogaming.com/wp-content/uploads/2019/02/Negative-Atmosphere-1-1038x576.jpg",
-  "https://www.relyonhorror.com/wp-content/uploads/2016/09/syndrome.jpg",
-  "https://onlysp.escapistmagazine.com/wp-content/uploads/2016/08/Syndrome-Banner-800x400.jpg",
+  "https://www.dsogaming.com/wp-content/uploads/2019/02/Negative-Atmosphere-1-1038x576.jpg", //Battle Background
+  "https://www.relyonhorror.com/wp-content/uploads/2016/09/syndrome.jpg", //Battle Background
+  "https://onlysp.escapistmagazine.com/wp-content/uploads/2016/08/Syndrome-Banner-800x400.jpg", //Battle Background
 ];
 const directionCards = [
   "https://i.imgur.com/fJqvBNO.png", //Forward
   "https://i.imgur.com/XzulilU.png", //Right
   "https://i.imgur.com/VXorAnC.png", //Left
 ];
+const currentMazeImage = [];
+const enemyInCombat = [];
+const enemyGraveyard = [];
 
 const $hand = $("#hand");
 const $mainScreen = $("#main-screen");
 const $startScreen = $("#start-screen");
 const $startButton = $("#start-button");
+const $attackButton = $("#attack-button");
+const $runButton = $("#run-button");
+const $combatButtonBox = $("#combat-button-box");
 
 const $randomDirectionCard = () =>
   $("<img>").attr(
@@ -51,10 +57,27 @@ const $randomDirectionCard = () =>
   );
 
 const chosenDirection = () => {
+  const mazeImage = mazeImages[Math.floor(Math.random() * mazeImages.length)];
   const $randomMazeImage = $("<img>")
-    .attr("src", mazeImages[Math.floor(Math.random() * mazeImages.length)])
+    .attr("src", mazeImage)
     .addClass("maze-image");
+  currentMazeImage.unshift(mazeImage);
   $($mainScreen).append($randomMazeImage);
+  if (currentMazeImage.length === 2) {
+    currentMazeImage.pop();
+  }
+
+  if (
+    currentMazeImage[0] === mazeImages[3] ||
+    currentMazeImage[0] === mazeImages[4] ||
+    currentMazeImage[0] === mazeImages[5]
+  ) {
+    generateEnemy();
+    combat();
+  } else {
+    $combatButtonBox.hide();
+    $(".attacked").text("");
+  }
 };
 
 const createHand = () => {
@@ -72,7 +95,23 @@ const createHand = () => {
   }
 };
 
-const hero = new Character(5, "Cody");
+const generateEnemy = () => {
+  const enemy = new Character(100, 5, "enemy");
+  enemyInCombat.unshift(enemy);
+}
+
+const combat = () => {
+  $combatButtonBox.show();
+  $(".attacked").text('Your Being Attacked!');
+  // enemyInCombat[0].attack(hero);
+  $attackButton.on("click", () => {
+    hero.attack(enemyInCombat[0]);
+    console.log(hero.hp);
+    console.log(enemyInCombat[0].hp);
+  });
+};
+
+const hero = new Character(100, 5, "Cody");
 
 $startButton.on("click", () => {
   $startScreen.remove();
@@ -80,4 +119,7 @@ $startButton.on("click", () => {
   chosenDirection();
 });
 
-$(() => {});
+
+$(() => {
+  $combatButtonBox.hide();
+});
