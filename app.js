cf752e1155b1838d67e8e3d96fc29939da78ca4d
Cody Usher <usher.cody@outlook.com>
2020-12-10 22:19:05 -0700

    balanced game

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const mazeImages = [
   "defaultImages/hallwayThree.jpg",
   "defaultImages/battleImageOne.jpg",
   "defaultImages/battleImageTwo.jpg",
-  "defaultImages/battleImageThree.jpg"
+  "defaultImages/battleImageThree.jpg",
 ];
 const directionCards = [
   "defaultImages/pointUpCard.png", //Forward
@@ -40,7 +40,7 @@ const directionCards = [
 const currentMazeImage = [];
 const enemyInCombat = [];
 const enemyGraveyard = [];
-let hero = '';
+let hero = "";
 let isClicked = false;
 
 const $hand = $("#hand");
@@ -50,10 +50,16 @@ const $gameOverScreen = $("#game-over-screen");
 const $startButton = $("#start-button");
 const $attackButton = $("#attack-button");
 const $runButton = $("#run-button"); //take away energy can only run 4 times.
-const $restartButton = $('#restart-button');
+const $restartButton = $("#restart-button");
 const $combatButtonBox = $("#combat-button-box");
-const $bossBattleImage = $("<img>").attr("src", "defaultImages/bossBattleImage.jpeg");
-const $gameOverImage = $("<img>").attr("src", "defaultImages/gameOverImage.jpg");
+const $bossBattleImage = $("<img>").attr(
+  "src",
+  "defaultImages/bossBattleImage.jpeg"
+);
+const $gameOverImage = $("<img>").attr(
+  "src",
+  "defaultImages/gameOverImage.jpg"
+);
 const $enemyImage = $("<img>").attr("src", "defaultImages/enemy.png");
 const $escapePodImage = $("<img>").attr("src", "defaultImages/escapePod.png");
 
@@ -88,20 +94,19 @@ const chosenDirection = () => {
     $enemyImage.hide();
   }
 
-  if (enemyGraveyard.length === 6) {
-    console.log(enemyGraveyard);
-    $('.maze-image').remove();
+  if (enemyGraveyard.length === 4) {
+    $(".maze-image").remove();
     $enemyImage.hide();
     $mainScreen.append($bossBattleImage);
     $bossBattleImage.show();
+    $combatButtonBox.show();
     generateBoss();
     combat();
-    console.log(enemyGraveyard.length);
   }
 
-  if (enemyGraveyard.length > 6) {
+  if (enemyGraveyard.length > 4) {
     $bossBattleImage.hide();
-    $('.maze-image').remove();
+    $(".maze-image").remove();
     $hand.hide();
     $combatButtonBox.hide();
     $enemyImage.hide();
@@ -122,52 +127,57 @@ const createHand = () => {
         $(".maze-image").remove();
         chosenDirection();
         hero.drawCard();
+        isClicked = false;
       })
       .append($randomDirectionCard())
       .appendTo($hand);
   }
-  
 };
 
 const generateEnemy = () => {
-  const enemy = new Character(10, 2, "enemy");
+  const enemy = new Character(
+    15, Math.floor(Math.random() * 6) + 1,
+    "enemy"
+  );
   enemyInCombat.unshift(enemy);
-}
+};
 
 const generateBoss = () => {
-  const boss = new Character(10, 2, "boss");
+  const boss = new Character(Math.floor(Math.random() * 40) + 25, 10, "boss");
   enemyInCombat.unshift(boss);
-}
+};
 
 const generateHero = () => {
-   hero = new Character(10, 5, "Cody");
-}
+  hero = new Character(95, 10, "Cody");
+};
 
 const combat = () => {
   $combatButtonBox.show();
-  isClicked = false;
-  $hand.hide()
-  console.log("HP" + hero.hp);
+  $hand.hide();
   console.log(isClicked);
-  $(".attacked").text('Your Being Attacked!').addClass("attack-announce").appendTo($combatButtonBox);
+  $(".attacked")
+    .text("Your Being Attacked!")
+    .addClass("attack-announce")
+    .appendTo($combatButtonBox);
   if (isClicked === true) {
     enemyInCombat[0].attack(hero);
   }
   if (hero.hp <= 0) {
     $("<h1>").text("You Died!").addClass("died").appendTo($gameOverScreen);
     $combatButtonBox.hide();
-    $('.maze-image').hide();
+    $(".maze-image").hide();
     $enemyImage.hide();
+    $bossBattleImage.hide();
     $hand.children().remove();
     $restartButton.show();
     $gameOverImage.appendTo($gameOverScreen);
     $gameOverScreen.show();
   }
-  
+
   if (enemyInCombat[0].hp <= 0) {
     for (index of enemyInCombat) {
       enemyGraveyard.push(index);
-    };
+    }
     $enemyImage.hide();
     enemyInCombat.pop();
     $combatButtonBox.hide();
@@ -188,6 +198,7 @@ $attackButton.on("click", () => {
   hero.attack(enemyInCombat[0]);
   isClicked = true;
   combat();
+  console.log(hero.hp);
 });
 
 $runButton.on("click", () => {
@@ -198,10 +209,13 @@ $runButton.on("click", () => {
     $hand.show();
     chosenDirection();
   } else {
-    $("<h1>").text("Your out of energy! You must fight!").addClass("energy-announce").appendTo($combatButtonBox);
+    $("<h1>")
+      .text("Your out of energy! You must fight!")
+      .addClass("energy-announce")
+      .appendTo($combatButtonBox);
     $(".attack-announce").hide();
     $runButton.hide();
-  };
+  }
 });
 
 $restartButton.on("click", () => {
